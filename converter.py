0977cd21f67c3f62d57e024b6e2ec6e0e239ef53
PiggyAwesome <48888771+PiggyAwesome@users.noreply.github.com>
2022-07-18 15:53:07 +0200

    working version 1
    now formats the headers correctly, also sorts out spaces in between items and values (keys)

--- converter.py
+++ converter.py
@@ -11,4 +11,13 @@ def convert(headers :str):
         good_last_line_headers = quoted_headers + f"{quote}"
     else:
         good_last_line_headers = quoted_headers[:-2]
-    return good_last_line_headers
+
+    separate_headers = good_last_line_headers.split("\n")
+    good_item_headers = ""
+    for header in separate_headers:
+        separate_split_headers = header.split(":")
+        good_item_headers = good_item_headers + separate_split_headers[0] + (quote + ": " + quote + "".join(separate_split_headers[1:])).replace(f"{quote}: {quote} ", f"{quote}: {quote}") + "\n"
+
+    good_item_headers = good_item_headers[:-1]
+
+    return good_item_headers
--- test_converter.py
+++ test_converter.py
@@ -1,5 +1,5 @@
 import converter
 
-output = converter.convert('hello: jhsujissdd\nhello2: 202092\n\n\n\n\n\n\n\n')
+output = converter.convert('hello:jhsujissdd\nhello2:202092\nhello3:202s092\n\n\n\n\n\n')
 
 print(output)
